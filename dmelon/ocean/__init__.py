70aeec4ee27981de06ac3b7c0728f58d05c5b83e
Gerardo Rivera Tello <gerardo_art@me.com>
2020-11-30 17:40:49 -0500

    Add module with ocean-related stuff

---
+++ dmelon/ocean/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from .core import DispersionRelation
+
+__all__ = ["DispersionRelation"]
---
+++ dmelon/ocean/core.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+import numpy as np
+
+
+class DispersionRelation:
+    @staticmethod
+    def low_freq(m):
+        BETA = 2.29e-11
+        C = 2.7
+        CONST1 = (BETA * C) ** (1 / 2)
+        CONST2 = (BETA / C) ** (1 / 2)
+        k = np.arange(-10, 10, 0.1)
+        w = -k / (2 * m + 1 + k ** 2)
+        return w * CONST1 * 60 * 60 * 24 / (2 * np.pi), k * CONST2 * 110e3 / (
+            2 * np.pi
+        )
