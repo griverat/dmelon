ef57bcf1ea56181e72a999d5fcc241869f2045aa
Gerardo Rivera Tello <gerardo_art@me.com>
2020-12-01 05:14:09 -0500

    Remove bm95 from __all__

--- dmelon/__init__.py
+++ dmelon/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from . import bm95, ocean, spectral, statistics
+from . import ocean, spectral, statistics
 
 """Top-level package for DMelon."""
 
-__all__ = ["bm95", "ocean", "spectral", "statistics"]
+__all__ = ["ocean", "spectral", "statistics"]
 __author__ = """Gerardo A. Rivera Tello"""
 __email__ = "gerardo_art@me.com"
 __version__ = "0.1.1"
